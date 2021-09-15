8cbb572b95426da5c4773a11c274f58cf6fa6880
lycstar <1173285195@qq.com>
2021-09-15 15:04:18 +0800

    add action secrets

--- bin/dukou_chekin.dart
+++ bin/dukou_chekin.dart
@@ -1,20 +1,49 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:dio/dio.dart';
 
 void main(List<String> arguments) async {
-  var tokenResponse = await Dio().post(
+  var email = Platform.environment['EMAIL_KEY'];
+  var passwd = Platform.environment['PASSWD_KEY'];
+  var serverKey = Platform.environment['SERVER_KEY'];
+
+  if (email != null && passwd != null) {
+    var token = await login(email, passwd);
+    var message = await checkin(token);
+    if (serverKey != null) {
+      await sendCheckinMessage(serverKey, message);
+    }
+  }
+}
+
+Future<String> login(String email, String passwd) async {
+  var response = await Dio().post(
     'https://dukouapi.com/api/token',
     data: {
-      'email': '1532628007@qq.com',
-      'passwd': 'a19981127',
+      'email': email,
+      'passwd': passwd,
     },
   );
-  var map = jsonDecode(tokenResponse.data);
-  var checkinResponse = await Dio(BaseOptions(
+  var map = jsonDecode(response.data);
+  return map['token'];
+}
+
+Future<String> checkin(String token) async {
+  var response = await Dio(BaseOptions(
     headers: {
-      'access-token': map['token'],
+      'access-token': token,
     },
   )).get('https://dukouapi.com/api/user/checkin');
-  print(checkinResponse.data);
+  return response.data.toString();
+}
+
+Future<void> sendCheckinMessage(String serverKey, String msg) async {
+  await Dio().get(
+    'https://sctapi.ftqq.com/$serverKey.send',
+    queryParameters: {
+      'title': '渡口签到结果',
+      'desp': msg,
+    },
+  );
 }
