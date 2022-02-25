cb4464c50819160f84c3d810b779b13eea5a878b
lycstar <1173285195@qq.com>
2022-02-25 11:58:10 +0800

    添加流量转换

--- bin/dukou_chekin.dart
+++ bin/dukou_chekin.dart
@@ -3,6 +3,19 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 
+class CheckinResult {
+  int ret;
+  String result;
+
+  CheckinResult({
+    required this.ret,
+    required this.result,
+  });
+
+  factory CheckinResult.fromJson(Map<String, dynamic> json) =>
+      CheckinResult(ret: json['ret'], result: json['result']);
+}
+
 void main(List<String> arguments) async {
   var email = Platform.environment['EMAIL_KEY'];
   var passwd = Platform.environment['PASSWD_KEY'];
@@ -10,8 +23,18 @@ void main(List<String> arguments) async {
 
   if (email != null && passwd != null) {
     var token = await login(email, passwd);
-    var message = await checkin(token);
-    print(message);
+    var result = await checkin(token);
+    var message = "";
+    if (result.ret == 1) {
+      String transformNum = await trafficTransform(result);
+      if (transformNum.isNotEmpty) {
+        message = '签到获得流量${transformNum}MB，转换流量成功';
+      } else {
+        message = "签到获得流量${transformNum}MB，转换流量失败";
+      }
+    } else {
+      message = "不能重复签到";
+    }
     if (serverKey != null) {
       await sendCheckinMessage(serverKey, message);
     }
@@ -30,13 +53,29 @@ Future<String> login(String email, String passwd) async {
   return map['token'];
 }
 
-Future<String> checkin(String token) async {
+Future<CheckinResult> checkin(String token) async {
   var response = await Dio(BaseOptions(
     headers: {
       'access-token': token,
     },
   )).get('https://dukouapi.com/api/user/checkin');
-  return response.data.toString();
+  print(response.data);
+  return CheckinResult.fromJson(response.data);
+}
+
+Future<String> trafficTransform(CheckinResult result) async {
+  RegExp regExp = RegExp(r"\d+");
+  var match = regExp.firstMatch(result.result);
+  var num = match?.group(0);
+  if (num != null && num.isNotEmpty) {
+    var response = await Dio().get(
+      'https://dukou.dev/api/user/koukanntraffic',
+      queryParameters: {'traffic': num},
+    );
+    print(response.data);
+    return num;
+  }
+  return "";
 }
 
 Future<void> sendCheckinMessage(String serverKey, String msg) async {
